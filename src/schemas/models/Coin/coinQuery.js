20afb7f977e9cc9dfea0380a974ed7de74e54788
Stefan Dorresteijn <stefan@refreshseo.nl>
2017-05-17 11:48:17 +0200

    Simplify getData function

--- src/schemas/models/Coin/coinQuery.js
+++ src/schemas/models/Coin/coinQuery.js
@@ -10,11 +10,11 @@ import Coin from './coinSchema'
 
 
 const getData = function _getData() {
-    const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=20'
-    return rp(url)
-        .then(htmlString => new Promise((resolve) => {
-            resolve(JSON.parse(htmlString))
-        }))
+    const options = {
+        url: 'https://api.coinmarketcap.com/v1/ticker/?limit=20',
+        json: true,
+    }
+    return rp(options)
 }
 
 export default {
