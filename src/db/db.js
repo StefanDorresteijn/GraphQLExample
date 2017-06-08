8974a060b6f1c5f04d65266e5b9f144e56c96b37
Stefan Dorresteijn <stefan@refreshseo.nl>
2017-06-08 16:31:19 +0200

    Add DB URL

--- src/db/db.js
+++ src/db/db.js
@@ -3,4 +3,4 @@ import { Sequelize } from 'sequelize'
 /**
  * We simply export the DB connection here. We'll use this to perform actions on the DB later.
  */
-export default new Sequelize('postgres://pgadmin:pgadmin@localhost:5432/graphql')
+export default new Sequelize(process.env.DB_URL)
